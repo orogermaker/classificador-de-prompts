87520a22bea46d6555b66e60913da3769c86a499
orogermaker <orogermaker@gmail.com>
2025-07-07 21:38:21 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // O @ vai apontar para src/
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
